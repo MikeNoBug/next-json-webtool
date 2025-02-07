d3ec793a3a5d95dcd060800edebab8851f22a595
wangweiqiang <wangweiqiang@lensung.com>
2025-02-07 09:52:50 +0800

    访问统计适配修改

--- src/components/service/statistic.tsx
+++ src/components/service/statistic.tsx
@@ -1,17 +1,29 @@
+'use client';
 import { getStaticsCount, saveStatics } from '@/request/statistic';
+import { useMount } from 'ahooks';
+import { useState } from 'react';
 
-const Statistic: React.FC = async () => {
-  const staticRes = await getStaticsCount();
-  saveStatics();
+const Statistic: React.FC = () => {
+  const [dayilyCount, setDailyCount] = useState(0);
+  const [allCount, setAllCount] = useState(0);
+  useMount(async () => {
+    saveStatics();
+    const res = await getStaticsCount();
+    if (!res.success) {
+      return;
+    }
+    setAllCount(res.data.allCount);
+    setDailyCount(res.data.todayCount);
+  });
   return (
     <div className='ml-md'>
       <div className='row-start-center'>
         <div className='w-[84px] text-right'>今日访问量：</div>
-        <div>{staticRes.success ? staticRes.data.todayCount : 0}</div>
+        <div>{dayilyCount}</div>
       </div>
       <div className='row-start-center'>
         <div className='w-[84px] text-right'>总访问量：</div>
-        <div>{staticRes.success ? staticRes.data.allCount : 0}</div>
+        <div>{allCount}</div>
       </div>
     </div>
   );
